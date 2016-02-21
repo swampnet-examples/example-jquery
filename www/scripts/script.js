e0cf1a67d951d044d55ec94224f46c3fe032b8d8
Pete Whitby <petejw@theswamp.co.uk>
2016-02-21 19:25:15 +0000

    add content

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
     console.log("ready!");
     
-    // Highligh all all paragraphs under all 'bob' divs
-    $('div.bob p').addClass('highlight');
+    // bobify all paragraphs under all 'bob' divs
+    $('div.bob p').addClass('bobify');
+    
+    // Anything with a class of capture-mouseover, do something when we mouseover
+    $('.capture-mouseover').on('mouseover', function() {
+        $(this).addClass('highlight');
+    });
+    
+    $('.capture-mouseover').on('mouseleave', function() {
+        $(this).removeClass('highlight');
+    });
 });
