57cda3c7533582c7428abd1d74bf5d1049ec2bd9
Pete Whitby <petejw@theswamp.co.uk>
2016-02-21 19:54:27 +0000

    add content

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
     
     // Anything with a class of capture-mouseover, do something when we mouseover
     $('.capture-mouseover').on('mouseover', function() {
+        console.debug('mouseover');
         $(this).addClass('highlight');
     });
     
     $('.capture-mouseover').on('mouseleave', function() {
+        console.debug('mouseleave');
         $(this).removeClass('highlight');
     });
 });
